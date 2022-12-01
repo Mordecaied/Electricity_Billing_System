a50be357f977365f4e6e1803430475c2b5d81e27
Mordechai-Edery <motta.josef@gmail.com>
2022-12-01 04:39:50 -0500

    Added secure db connection

--- src/ebs/database/Conn.java
+++ src/ebs/database/Conn.java
@@ -1,20 +1,54 @@
 package ebs.database;
 
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
+import java.util.Properties;
 
 public class Conn {
 	public Connection connection;
 	public Statement statement;
+	
+	String driverClass = "";
+	String url = "";
+	String username = "";
+	String password = "";
+	
 	public Conn() {
 		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			connection = DriverManager.getConnection("jdbc:mysql:///ebs","root","wegoREAL23@@@");
+			
+			Properties prop = loadPropertiesFile();
+
+			driverClass = prop.getProperty("MYSQLJDBC.driver");
+			url = prop.getProperty("MYSQLJDBC.url");
+			username = prop.getProperty("MYSQLJDBC.username");
+			password = prop.getProperty("MYSQLJDBC.password");
+			
+			Class.forName(driverClass);
+			connection = DriverManager.getConnection(url, username, password);
 			statement = connection.createStatement();
+			
+			if (connection != null) {
+				System.out.println("connection created successfully using properties file");
+			}
+
+			else {
+				System.out.println(" unable to create connection");
+			}
 		}catch(Exception e){
 			System.out.println(e);
 		}
+	}
+	
+	public static Properties loadPropertiesFile() throws Exception {
+
+		Properties prop = new Properties();
+		InputStream in = new FileInputStream("config.properties");
+		prop.load(in);
+		in.close();
+		return prop;
 	}
 
 }
--- src/ebs/testSuite/testConnection.java
+++ src/ebs/testSuite/testConnection.java
@@ -1,38 +1,61 @@
 package ebs.testSuite;
 
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Properties;
 
 public class testConnection {
 
-		static Connection connection;
-		static Statement statement;
+	public Connection connection;
+	public Statement statement;
+	
+	String driverClass = "";
+	String url = "";
+	String username = "";
+	String password = "";
 		
-		public testConnection() {
-			try {
-				Class.forName("com.mysql.cj.jdbc.Driver");
-				connection = DriverManager.getConnection("jdbc:mysql:///ebs","root","wegoREAL23@@@");
-				statement = connection.createStatement();
-			}catch(Exception e){
-				System.out.println(e);
-			}
-		}
-		
-		public static void testIt() {
-			try {
-				Class.forName("com.mysql.cj.jdbc.Driver");
-				connection = DriverManager.getConnection("jdbc:mysql:///ebs","root","wegoREAL23@@@");
-				statement = connection.createStatement();
-			}catch(Exception e){
-				System.out.println(e);
-			}
-			System.out.println("Connection succesfull");
-		}
+	public void testConnection() {
+		try {
+			
+			Properties prop = loadPropertiesFile();
 
+			driverClass = prop.getProperty("MYSQLJDBC.driver");
+			url = prop.getProperty("MYSQLJDBC.url");
+			username = prop.getProperty("MYSQLJDBC.username");
+			password = prop.getProperty("MYSQLJDBC.password");
+			
+			Class.forName(driverClass);
+			connection = DriverManager.getConnection(url, username, password);
+			statement = connection.createStatement();
+			
+			if (connection != null) {
+				System.out.println("connection created successfully using properties file");
+			}
+
+			else {
+				System.out.println(" unable to create connection");
+			}
+		}catch(Exception e){
+			System.out.println(e);
+		}
+	}
+	
+	public static Properties loadPropertiesFile() throws Exception {
+
+		Properties prop = new Properties();
+		InputStream in = new FileInputStream("config.properties");
+		prop.load(in);
+		in.close();
+		return prop;
+	}
 
 	public static void main(String[] args) {
-		testIt();
+		testConnection tc = new testConnection();
+		tc.testConnection();
 
 	}
 
